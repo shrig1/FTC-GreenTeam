a2384748bd05945379876cc0b5f3c9de781c7a94
NumberRaptor482 <49574286+NumberRaptor482@users.noreply.github.com>
2019-11-02 11:27:25 -0500

    Update AutonomousFinal.java

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFinal.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFinal.java
@@ -6,7 +6,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
 
 
 @Autonomous(name="Auto Finale", group="Iterative Opmode")
