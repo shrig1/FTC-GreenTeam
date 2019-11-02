07d7247b3363521cd4771f39132942750b75c68f
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2019-11-02 10:16:13 -0500

    Update AutonomousFinal.java

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFinal.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFinal.java
@@ -7,19 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-/**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
+
 
 @Autonomous(name="Auto Finale", group="Iterative Opmode")
 public class Auto_Finale extends OpMode
@@ -35,25 +23,19 @@ public class Auto_Finale extends OpMode
     public Servo servo2 = null;
     public Servo servo3 = null;
     public Servo servo4 = null;
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
+
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
+       
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
-        // Tell the driver that initialization is complete.
+       
         telemetry.addData("Status", "Initialized");
     }
 
