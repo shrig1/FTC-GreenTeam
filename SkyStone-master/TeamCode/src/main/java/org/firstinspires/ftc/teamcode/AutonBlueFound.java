32c7ff2af5c24d2818a7da2e892effc5d0696976
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2019-11-07 19:07:28 -0600

    Update AutonBlueFound.java

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonBlueFound.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonBlueFound.java
@@ -13,8 +13,8 @@ import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.SwitchableLight;
 
 
-@Autonomous(name="Auto Finale", group="Iterative Opmode")
-public class AutoFinal extends OpMode 
+@Autonomous(name="AutoBlueFound", group="Iterative Opmode")
+public class AutonBlueFound extends OpMode 
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -46,8 +46,8 @@ public class AutoFinal extends OpMode
         rightIntakePivot = hardwareMap.get(Servo.class, "right_intake_pivot");
         
         
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
         
         leftIntake.setDirection(DcMotor.Direction.REVERSE);
         
@@ -63,6 +63,8 @@ public class AutoFinal extends OpMode
  
     @Override
     public void init_loop() {
+        double i = 0;
+        double x = 500; 
     }
     @Override
     public void start() {
@@ -70,10 +72,133 @@ public class AutoFinal extends OpMode
     }
     @Override
     public void loop() {
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);        
-        leftDrive.setTargetPosition(1833);
-        rightDrive.setTargetPosition(1833);
+        // leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // leftDrive.setTargetPosition(1000);
+        // rightDrive.setTargetPosition(1000);
+        
+       /*if(i<x){
+           leftDrive.setPower(1);
+           rightDrive.setPower(1);
+       }
+        */
+        
+        
+        
+        
+        //Go forward till we reach the middle of teh two foundations
+        
+    //     leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    //     rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);        
+    //     leftDrive.setTargetPosition(2750);
+    //     rightDrive.setTargetPosition(2750);
+    
+    // if(rightDrive.getCurrentPosition() < rightDrive.getTargetPosition() && leftDrive.getCurrentPosition() < leftDrive.getTargetPosition()) {
+
+        
+    //     leftDrive.setPower(1);
+    //     rightDrive.setPower(1);
+    // }
+    // leftDrive.setPower(0);
+    // rightDrive.setPower(0);
+    
+    
+    
+    
+    //Turn 90 degrees left
+    
+    
+    
+    
+    // leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    // rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);        
+    // leftDrive.setTargetPosition(-900);
+    // rightDrive.setTargetPosition(900);
+    
+    // if(rightDrive.getCurrentPosition() < rightDrive.getTargetPosition() && leftDrive.getCurrentPosition() > leftDrive.getTargetPosition()){
+    //     leftDrive.setPower(-1);
+    //     rightDrive.setPower(1);
+        
+    // }
+    // leftDrive.setPower(0);
+    // rightDrive.setPower(0);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    // rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);        
+    //     leftDrive.setTargetPosition(900);
+    //     rightDrive.setTargetPosition(900);
+    
+    // if(rightDrive.getCurrentPosition() < rightDrive.getTargetPosition() && leftDrive.getCurrentPosition() < leftDrive.getTargetPosition()) {
+
+        
+    //     leftDrive.setPower(1);
+    //     rightDrive.setPower(1);
+    // }
+    // leftDrive.setPower(0);
+    // rightDrive.setPower(0);
+    
+    /*
+    //Turn 40 degrees left
+    leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);        
+        leftDrive.setTargetPosition(-NUMBER);
+        rightDrive.setTargetPosition(NUMBER);
+        
+    while(rightDrive.getCurrentPosition() < rightDrive.getTargetPosition() && leftDrive.getCurrentPosition() < leftDrive.getTargetPosition()) {
+
+        
+        leftDrive.setPower(-1);
+        rightDrive.setPower(1);
+    }
+    leftDrive.setPower(0);
+    rightDrive.setPower(0);
+        
+
+    
+    //push to building zone
+    
+    leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);        
+        leftDrive.setTargetPosition(NUMBER);
+        rightDrive.setTargetPosition(NUMBER);
+        
+    while(rightDrive.getCurrentPosition() < rightDrive.getTargetPosition() && leftDrive.getCurrentPosition() < leftDrive.getTargetPosition()) {
+
+        
+        leftDrive.setPower(1);
+        rightDrive.setPower(1);
+    }
+    leftDrive.setPower(0);
+    rightDrive.setPower(0);
+      
+    //turn 40 degrees left
+    leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);        
+        leftDrive.setTargetPosition(-NUMBER);
+        rightDrive.setTargetPosition(NUMBER);
+        
+    while(rightDrive.getCurrentPosition() < rightDrive.getTargetPosition() && leftDrive.getCurrentPosition() < leftDrive.getTargetPosition()) {
+
+        
+        leftDrive.setPower(-1);
+        rightDrive.setPower(1);
+    }
+    leftDrive.setPower(0);
+    rightDrive.setPower(0);
+    
+    //move forward
+    leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);        
+        leftDrive.setTargetPosition(NUMBER);
+        rightDrive.setTargetPosition(NUMBER);
     
     while(rightDrive.getCurrentPosition() < rightDrive.getTargetPosition() && leftDrive.getCurrentPosition() < leftDrive.getTargetPosition()) {
 
@@ -83,32 +208,28 @@ public class AutoFinal extends OpMode
     }
     leftDrive.setPower(0);
     rightDrive.setPower(0);
-    /*
-    INSERT CODE TO TURN HERE. 
     
-    INSERT CODE TO GRAB BUILD SITE HERE.
-    
-    CODE TO MOVE BACKWARDS TO BUILDING ZONE.
-        leftDrive.setTargetPosition(INSERT NUMBER HERE);
-        rightDrive.setTargetPosition(INSERT NUMBER HERE);
         
+    
+    //park underneath bridge
+    leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);        
+        leftDrive.setTargetPosition(NUMBER);
+        rightDrive.setTargetPosition(NUMBER);
+    
     while(rightDrive.getCurrentPosition() < rightDrive.getTargetPosition() && leftDrive.getCurrentPosition() < leftDrive.getTargetPosition()) {
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         
-        leftDrive.setPower(-1);
-        rightDrive.setPower(-1);
+        leftDrive.setPower(1);
+        rightDrive.setPower(1);
     }
-    
     leftDrive.setPower(0);
     rightDrive.setPower(0);
     
-    INSERT CODE TO PARK UNDERNEATH BRIDGE.
+        
+    */
     
-    */    
-
-    }
-
+}
     @Override
     public void stop() {
     }
