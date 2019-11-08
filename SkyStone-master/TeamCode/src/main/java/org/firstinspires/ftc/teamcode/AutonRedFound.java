57338618a67a28339d0117fe43c86e29197475cb
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2019-11-08 17:54:18 -0600

    Update AutonRedFound.java

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRedFound.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRedFound.java
@@ -5,22 +5,20 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-
-
-@Autonomous(name="AutoBlueFound", group="Linear Opmode")
-public class AutonBlueFound extends LinearOpMode 
+@Autonomous(name="AutoRedFound", group="Linear Opmode")
+public class AutonRedFound extends LinearOpMode 
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
-   
-
     
-
-    double ticks = 0;
-    
-
+	// Put stage timings at top of script to make config easy
+    double ticks = 0; // DO NOT EDIT
+    double firstStage  = 0;
+	double secondStage = firstStage + 8000;
+	double thirdStage  = secondStage + 4000;
+    double fourthStage = thirdStage + 0;
     
     @Override
     public void init() {
@@ -30,75 +28,30 @@ public class AutonBlueFound extends LinearOpMode
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
         
-        rightIntake = hardwareMap.get(DcMotor.class, "right_intake");
-        leftIntake = hardwareMap.get(DcMotor.class, "left_intake");
-        
-        leftIntakePivot = hardwareMap.get(Servo.class, "left_intake_pivot");
-        rightIntakePivot = hardwareMap.get(Servo.class, "right_intake_pivot");
-        
-        
+      
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
-        
-        leftIntake.setDirection(DcMotor.Direction.REVERSE);
-        
-        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightIntake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftIntake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        
-    }
-
- 
-    @Override
-    public void init_loop() {
-    
-    }
-    @Override
-    public void start() {
-        runtime.reset();
-    }
-    @Override
-    public void loop() {
-      
-      
-      while(opModeIsActive()){
-      
-      
-      ticks++;
-        
-        
-        if(ticks > 0 && ticks < ticks*0.5){
-            leftDrive.setPower(1);
-            rightDrive.setPower(1);
-            
-        }
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
-        
-        if(ticks > ticks*0.5 && ticks < ticks*1.2){
-            leftDrive.setPower(1);
-            rightDrive.setPower(-1);
-        }
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
-        if(ticks > ticks*1.2 && ticks < ticks*2.5){
-            leftDrive.setPower(1);
-            rightDrive.setPower(1);
-        }
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
-        
-      }
-        
-        
-        
-        
-        
-    
+	}
 }
-    @Override
-    public void stop() {
-    }
 
+@Override
+void loop() {    
+	// run until the end of the match (driver presses STOP)
+    while (opModeIsActive()) {
+		ticks++;
+		// Show the elapsed game time and wheel power.
+		if(ticks > firstStage && ticks < secondStage) {
+			leftDrive.setPower(1.0);
+			rightDrive.setPower(1.0);
+		} else if(ticks > secondStage && ticks < thirdStage) {
+			leftDrive.setPower(1.0);
+			rightDrive.setPower(-1.0);
+        } else if(ticks > thirdStage && ticks < fourthStage) {
+			leftDrive.setPower(1.0);
+			rightDrive.setPower(1.0);
+		} else {
+			leftDrive.setPower(0.0);
+			rightDrive.setPower(0.0);
+		}
+	}
 }
