ca8d4aae7e431d09e54738c4200059b838af6735
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2019-11-08 17:55:35 -0600

    Update AutonRedSky.java

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRedSky.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRedSky.java
@@ -1,97 +1,105 @@
 package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpmode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
-import com.qualcomm.robotcore.hardware.NormalizedRGBA;
-import com.qualcomm.robotcore.hardware.SwitchableLight;
 
+@Autonomous(name="AutoRedSky", group="Linear Opmode")
+public class AutonRedSky extends LinearOpMode 
 
-@Autonomous(name="Auto Finale", group="Iterative Opmode")
-public class AutoFinal extends OpMode 
 {
+
     // Declare OpMode members.
+
     private ElapsedTime runtime = new ElapsedTime();
+
     private DcMotor leftDrive = null;
+
     private DcMotor rightDrive = null;
-    private DcMotor rightIntake = null;
-    private DcMotor leftIntake = null;
-
-    public Servo leftIntakePivot = null;
-    public Servo rightIntakePivot = null;
-    NormalizedColorSensor colorSensor = null;
-    
-    public static final int coreHexEncode = 288;
-    public static final int dcMotorEncode = 2240;
 
     
+
+	// Put stage timings at top of script to make config easy
+
+    double ticks = 0; // DO NOT EDIT
+
+    double firstStage  = 0;
+
+	double secondStage = firstStage + 8000;
+
+	double thirdStage  = secondStage + 4000;
+
+    double fourthStage = thirdStage + 0;
+
+    
+
     @Override
+
     public void init() {
+
         telemetry.addData("Status", "Initialized");
 
+
+
        
+
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-        
-        rightIntake = hardwareMap.get(DcMotor.class, "right_intake");
-        leftIntake = hardwareMap.get(DcMotor.class, "left_intake");
-        
-        leftIntakePivot = hardwareMap.get(Servo.class, "left_intake_pivot");
-        rightIntakePivot = hardwareMap.get(Servo.class, "right_intake_pivot");
-        
-        
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
-        
-        leftIntake.setDirection(DcMotor.Direction.REVERSE);
-        
-        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightIntake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftIntake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        
+
         
 
-    }
+      
 
- 
-    @Override
-    public void init_loop() {
-    }
-    @Override
-    public void start() {
-        runtime.reset();
-    }
-    @Override
-    public void loop() {
-        
-        leftDrive.setTargetPosition(1833);
-        rightDrive.setTargetPosition(1833);
-    
-    while(rightDrive.getCurrentPosition() < rightDrive.getTargetPosition() && leftDrive.getCurrentPosition() < leftDrive.getTargetPosition()) {
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        
-        leftDrive.setPower(1);
-        rightDrive.setPower(1);
-    }
-    leftDrive.setPower(0);
-    rightDrive.setPower(0);
-    
-    
-        
-        
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
 
-    }
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
-    @Override
-    public void stop() {
-    }
+	}
+
+}
+
+
+
+@Override
+
+void loop() {    
+
+	// run until the end of the match (driver presses STOP)
+
+    while (opModeIsActive()) {
+
+		ticks++;
+
+		// Show the elapsed game time and wheel power.
+
+		if(ticks > firstStage && ticks < secondStage) {
+
+			leftDrive.setPower(1.0);
+
+			rightDrive.setPower(1.0);
+
+		} else if(ticks > secondStage && ticks < thirdStage) {
+
+			leftDrive.setPower(1.0);
+
+			rightDrive.setPower(-1.0);
+
+        } else if(ticks > thirdStage && ticks < fourthStage) {
+
+			leftDrive.setPower(1.0);
+
+			rightDrive.setPower(1.0);
+
+		} else {
+
+			leftDrive.setPower(0.0);
+
+			rightDrive.setPower(0.0);
+
+		}
+
+	}
 
 }
